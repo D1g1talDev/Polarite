b9da5972737fc60050f44e4b3c2cc52bad44584c
D1g1tal <85529119+D1g1talDev@users.noreply.github.com>
2026-03-17 20:42:45 -0500

    disabled vending machines in fraud
    people could just lag your game using the vending machines in fraud

---
+++ JaketLite/Patches/CoinLag.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Polarite.Multiplayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polarite.Patches
+{
+    [HarmonyPatch(typeof(CoinActivated))]
+    internal class CoinLag
+    {
+        [HarmonyPatch(nameof(CoinActivated.OnTriggerEnter))]
+        [HarmonyPrefix]
+        static bool StopTrigger()
+        {
+            if (!NetworkManager.InLobby || SceneHelper.CurrentScene == "Level 1-1")
+            {
+                return true;
+            }
+            return !NetworkManager.InLobby;
+        }
+    }
+}
