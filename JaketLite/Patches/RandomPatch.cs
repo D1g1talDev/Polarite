64b01786185950f4867370e1e8c610e627228d09
D1g1tal <85529119+D1g1talDev@users.noreply.github.com>
2026-03-18 23:42:48 -0500

    make 8-3's first room always 0-2

---
+++ JaketLite/Patches/RandomPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Polarite.Multiplayer;
+using Randomness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Polarite.Patches
+{
+    [HarmonyPatch(typeof(RandomBase<RandomGameObjectEntry>))]
+    internal class RandomPatch
+    {
+        [HarmonyPatch(nameof(RandomBase<RandomGameObjectEntry>.WeightedPick))]
+        [HarmonyPrefix]
+        static bool Prefix(RandomBase<RandomGameObjectEntry> __instance, ref List<RandomGameObjectEntry> __0, ref RandomGameObjectEntry __result)
+        {
+            if(NetworkManager.InLobby && __0.Count > 0)
+            {
+                __result = __0[0];
+                return false;
+            }
+            return true;
+        }
+    }
+}
