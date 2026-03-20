de95d108812132d786298391abc85cf117926617
D1g1tal <85529119+D1g1talDev@users.noreply.github.com>
2026-03-20 13:10:33 -0500

    disable suicide trees for clients

---
+++ JaketLite/Patches/SuicideTreePatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polarite.Multiplayer;
+
+namespace Polarite.Patches
+{
+    [HarmonyPatch(typeof(EnemySpawnRadius))]
+    internal class SuicideTreePatch
+    {
+        [HarmonyPatch(nameof(EnemySpawnRadius.SpawnEnemy))]
+        [HarmonyPrefix]
+        static bool Prefix()
+        {
+            if(NetworkManager.InLobby)
+            {
+                return !NetworkManager.ClientAndConnected;
+            }
+            return true;
+        }
+    }
+}
