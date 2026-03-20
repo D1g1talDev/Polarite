62801b1a3e6ba777ab8d9e9a6422381e0506a205
D1g1tal <85529119+D1g1talDev@users.noreply.github.com>
2026-03-20 16:45:09 -0500

    fix being able to never skip cutscenes outside lobbies

--- JaketLite/Patches/SkipPatch.cs
+++ JaketLite/Patches/SkipPatch.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using HarmonyLib;
+using Polarite.Multiplayer;
 
 namespace Polarite.Patches
 {
@@ -15,8 +16,11 @@ namespace Polarite.Patches
         [HarmonyPostfix]
         static void Never(CutsceneSkip __instance)
         {
-            MonoSingleton<CutsceneSkipText>.Instance.Hide();
-            __instance.waitingForInput = false;
+            if(NetworkManager.InLobby)
+            {
+                MonoSingleton<CutsceneSkipText>.Instance.Hide();
+                __instance.waitingForInput = false;
+            }
         }
     }
 }
