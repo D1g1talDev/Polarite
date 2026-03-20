6cf3ec3dd57870ecc25fab81ff318365122d71b8
D1g1tal <85529119+D1g1talDev@users.noreply.github.com>
2026-03-20 13:05:12 -0500

    disable deathcatchers for clients

---
+++ JaketLite/Patches/DeathcatcherPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Polarite.Multiplayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polarite.Patches
+{
+    [HarmonyPatch(typeof(Deathcatcher))]
+    internal class DeathcatcherPatch
+    {
+        [HarmonyPatch(nameof(Deathcatcher.Update))]
+        [HarmonyPrefix]
+        static void Update(Deathcatcher __instance)
+        {
+            if(NetworkManager.InLobby)
+            {
+                __instance.active = !NetworkManager.ClientAndConnected;
+            } 
+        }
+    }
+}
