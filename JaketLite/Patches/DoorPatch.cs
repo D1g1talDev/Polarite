8358d3f99076e114b26ffdfbed20ec5beff2bb6c
D1g1tal <85529119+D1g1talDev@users.noreply.github.com>
2026-07-13 13:49:27 -0500

    oops

--- JaketLite/Patches/DoorPatch.cs
+++ JaketLite/Patches/DoorPatch.cs
@@ -24,7 +24,7 @@ namespace Polarite.Patches
                 bool found = false;
                 foreach (var p in NetworkManager.players.Values)
                 {
-                    if (Vector3.Distance(__instance.transform.position, p.transform.position) <= 50f)
+                    if (Vector3.SqrMagnitude(__instance.transform.position - p.transform.position) <= 50f)
                     {
                         found = true;
                         break;
